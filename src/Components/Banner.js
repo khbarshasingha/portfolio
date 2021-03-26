733c400568e48428b47ed080ce724ea849b33a9e
Kh Barsha Singha <khbarshasingha@gmail.com>
2021-03-26 13:57:10 +0530

    banner and projects

---
+++ src/Components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../cssFiles/banner.css";
+
+export const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="inner">
+        <div className="title">
+          <h2>Hi, This is Kh Barsha Singha</h2>
+          <h2>I am a Front-end Developer</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/Components/Projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../cssFiles/projects.css";
+
+export const Projects = () => {
+  return (
+    <div
+      className="projects
+    "
+    >
+      <div className="textd">
+        {" "}
+        <h3>PROJECTS</h3>
+      </div>
+    </div>
+  );
+};
